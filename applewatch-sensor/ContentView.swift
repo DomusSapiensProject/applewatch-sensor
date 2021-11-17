6340b338925ab744e0244064eb2d850b8e0d7c80
Andrea Santangelo <asantangelo90@gmail.com>
2021-11-17 12:39:19 +0100

    First commit: Hello World.

---
+++ applewatch-sensor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  applewatch-sensor
+//
+//  Created by andrea santangelo on 17/11/2021.
+//  Copyright © 2021 NetSense. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
